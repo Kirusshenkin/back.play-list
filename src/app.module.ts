ce87be7e284f3b34dfd7fcfeb3981f1c856a29f5
Pako <kirill.mitin34@gmail.com>
2021-11-20 21:21:57 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { FileModule } from './file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import * as path from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static')}),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI
+    ),
+    TrackModule,
+    FileModule,
+    AuthModule,
+    UsersModule
+  ],
+})
+export class AppModule {}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UserSchema } from 'src/track/schemas/user.schema';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt-auth.strategy';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
